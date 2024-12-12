1f567d6b1f26e10883216cc5fddcd2d6dd8cb408
Ejrosa9028 <emanueljavierrosa@gmail.com>
2024-12-12 11:27:16 -0400

    1. Insert left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_insert_left - Inserta un nodo como hijo izquierdo de otro nodo.
+ * @parent: Puntero al nodo donde se insertará el hijo izquierdo.
+ * @value: El valor que se almacenará en el nuevo nodo.
+ *
+ * Return: Puntero al nuevo nodo creado, o NULL si hay un error.
+ *
+ * Descripción: Si el padre ya tiene un hijo izqu, el new nodo toma su lugar.
+ * El hijo izquierdo debe ser asignado como hijo izquierdo del nuevo nodo.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	/* Verificar si parent es NULL */
+	if (!parent)
+	{
+		return (NULL);
+	}
+
+	/* Asignar memoria para el nuevo nodo */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	/* Inicializar el nuevo nodo */
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	/* Si el nodo padre ya tiene un hijo izquierdo */
+	if (parent->left)
+	{
+		/* Asignar el antiguo hijo izquierdo como hijo izquierdo del nuevo nodo */
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+
+	/* Asignar el nuevo nodo como hijo izquierdo del nodo padre */
+	parent->left = new_node;
+
+	return (new_node);
+}
